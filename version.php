336417566f34c5e230cf4ca517a6d507d532eff6
Mattan Dor <mattan.dor1@gmail.com>
2026-03-13 15:54:37 +0200

    Update version number to 1.2 in version.php

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ $plugin->version   = 2026031400;
 $plugin->requires  = 2017111300;
 $plugin->component = 'mod_streamassign';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0';
+$plugin->release   = '1.2';
 
 // local_stream is required (provides Stream URL and API key).
 $plugin->dependencies = [
