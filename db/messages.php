6364252fa5ff14492ec7f2311367a0ca296c5147
Mattan Dor <mattan.dor1@gmail.com>
2026-03-19 13:57:26 +0200

    Remove default message settings for submission provider in messages.php to simplify configuration.

--- db/messages.php
+++ db/messages.php
@@ -25,10 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $messageproviders = [
-    'submission' => [
-        'defaults' => [
-            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
-            'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
-        ],
-    ],
+    'submission' => [],
 ];
