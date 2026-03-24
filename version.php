dc463c0b3ed69608bf5d9bf5460dbdaf32261a81
Mattan Dor <mattan.dor1@gmail.com>
2026-03-24 16:39:57 +0200

    Update version number to 1.7 in version.php to reflect recent changes.

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ $plugin->version   = 2026032400;
 $plugin->requires  = 2017111300;
 $plugin->component = 'mod_streamassign';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.6';
+$plugin->release   = '1.7';
 
 // local_stream is required (provides Stream URL and API key).
 $plugin->dependencies = [
