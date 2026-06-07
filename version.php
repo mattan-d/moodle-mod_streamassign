f026079cab55ca4a802bbe4a4c116c794d6ff2af
Mattan Dor <mattan.dor1@gmail.com>
2026-06-07 11:10:30 +0300

    Update version to 2026060700 in version.php to reflect recent changes.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026052900;
+$plugin->version   = 2026060700;
 $plugin->requires  = 2017111300;
 $plugin->component = 'mod_streamassign';
 $plugin->maturity  = MATURITY_STABLE;
